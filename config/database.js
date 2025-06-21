ad3c59afdbd9ae4aeefbe2223c03aca5f9f161d4
John Doe <hieumonster8@gmail.com>
2025-06-21 09:28:51 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    url: 'mongodb+srv://tungtung:admindeptrai@ac-zlgw7r1.xsrqoxt.mongodb.net/RedVip?retryWrites=true&w=majority',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
