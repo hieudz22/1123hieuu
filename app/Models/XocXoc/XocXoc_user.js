d3a3b3fdb01f86d7f9915d6c0504ba175d0d8485
hieudz22 <hieumonster8@gmail.com>
2025-06-21 10:19:36 +0700

    Update XocXoc_user.js

--- app/Models/XocXoc/XocXoc_user.js
+++ app/Models/XocXoc/XocXoc_user.js
@@ -1,7 +1,11 @@
-let Schema = new mongoose.Schema({
-	uid:    {type: String, required: true, unique: true},
-	bet:    {type: Number, default: 0},
-	win:    {type: Number, default: 0, index: true},
-	lost:   {type: Number, default: 0, index: true},
-	totall: {type: Number, default: 0, index: true},
+const mongoose = require('mongoose');
+
+const Schema = new mongoose.Schema({
+    uid:    { type: String, required: true, unique: true },
+    bet:    { type: Number, default: 0 },
+    win:    { type: Number, default: 0, index: true },
+    lost:   { type: Number, default: 0, index: true },
+    totall: { type: Number, default: 0, index: true },
 });
+
+module.exports = mongoose.model('XocXoc_users', Schema);
