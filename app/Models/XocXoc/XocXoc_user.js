15fc8b736e075414de939189ca1d8e03260e86a8
hieudz22 <hieumonster8@gmail.com>
2025-06-21 10:17:40 +0700

    Update XocXoc_user.js

--- app/Models/XocXoc/XocXoc_user.js
+++ app/Models/XocXoc/XocXoc_user.js
@@ -1,12 +1,7 @@
-
-let mongoose = require('mongoose');
-
 let Schema = new mongoose.Schema({
-	uid:    {type: String, required: true, unique: true}, // ID Người chơi
-	bet:    {type: mongoose.Schema.Types.Long, default: 0}, // Số tiền đã chơi
-	win:    {type: mongoose.Schema.Types.Long, default: 0, index:true}, // Số tiền đã thắng
-	lost:   {type: mongoose.Schema.Types.Long, default: 0, index:true}, // Số Red đã thua
-	totall: {type: mongoose.Schema.Types.Long, default: 0, index:true}, // Thắng trừ thua
+	uid:    {type: String, required: true, unique: true},
+	bet:    {type: Number, default: 0},
+	win:    {type: Number, default: 0, index: true},
+	lost:   {type: Number, default: 0, index: true},
+	totall: {type: Number, default: 0, index: true},
 });
-
-module.exports = mongoose.model('XocXoc_users', Schema);
